ca445a172700952f213ed2db12305675a32e33b6
Karan <karansuggu@gmail.com>
2023-09-28 14:03:57 +0530

    two buttons to go to camera and google chrome (website).

---
+++ app/src/main/java/com/example/implicitintent/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<CardView>(R.id.cv1).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://github.com/Karangupta0786?tab=repositories")
+            startActivity(intent)
+        }
+
+        findViewById<CardView>(R.id.cv2).setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+
+
+    }
+}
