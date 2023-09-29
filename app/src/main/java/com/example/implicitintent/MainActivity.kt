7c3e04f1843e83b45de5334c027c469ac85fdb0c
Karan <karansuggu@gmail.com>
2023-09-29 22:09:15 +0530

    some Layout changes done.

--- app/src/main/java/com/example/implicitintent/MainActivity.kt
+++ app/src/main/java/com/example/implicitintent/MainActivity.kt
@@ -14,13 +14,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<CardView>(R.id.cv1).setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("https://github.com/Karangupta0786?tab=repositories")
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivity(intent)
         }
 
         findViewById<CardView>(R.id.cv2).setOnClickListener {
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.youtube.com/")
             startActivity(intent)
         }
         findViewById<Button>(R.id.btn_next).setOnClickListener {
